54b6967ff6812fb55374243bb926909c193783c7
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-16 14:40:31 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_actorid_to_character.rb
+++ db/migrate/006_add_actorid_to_character.rb
@@ -1,4 +1,4 @@
-class AddActorIDToCharacters < ActiveRecord::Migration[5.1]
+class AddActoridToCharacters < ActiveRecord::Migration[5.1]
 
   def change
     add_column :characters, :actor_id, :integer
