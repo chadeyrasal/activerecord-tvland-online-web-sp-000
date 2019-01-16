f7d1f3f2654fab2991f1deaa531cdb2f2888740e
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-16 14:58:42 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_name_day_season_to_show.rb
+++ db/migrate/009_add_name_day_season_to_show.rb
@@ -2,7 +2,7 @@ class AddNameDaySeasonToShow < ActiveRecord::Migration[5.1]
 
   def change
     add_column :shows, :day, :string
-    add_column :shows, :season, :integer
+    add_column :shows, :season, :string
   end
 
 end
