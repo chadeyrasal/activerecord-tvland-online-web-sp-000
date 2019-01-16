bfd278ff9b182f7de98a290cc75ca7030c521651
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-16 14:43:38 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,8 +3,6 @@ class Actor < ActiveRecord::Base
   has_many :characters
 
   def full_name
-    firstname = self.first_name
-    lastname = self.last_name
     fullname = "#{self.first_name} #{self.last_name}"
   end
 
